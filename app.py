5a7619adfa23b30426a68d301dfe2326795564b0
Shino Shaji <102040447+Shino-Shaji@users.noreply.github.com>
2023-12-19 17:20:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
-import processdata, helper
+import processdata
+import helper
 import trial
+import numpy
 
 st.sidebar.title("WhatsInsight")
 
